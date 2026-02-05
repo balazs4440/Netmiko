c38c15b65702926f0d4967a031be3672114b4815
balazs4440 <mariapartics@gmail.com>
2026-02-05 09:09:07 +0100

    feladat_002

---
+++ asd.py
@@ -0,0 +1,34 @@
+from netmiko import ConnectHandler
+
+login_adatok = {
+    "device_type": "cisco_ios",
+    "host": "192.168.40.219",
+    "username": "balazs",
+    "password": "sshP455"
+}
+
+
+def vlan(ssh):
+    vlanok = ("vlan 10",
+              "name Tanulo",
+              "vlan 20",
+              "name Oktato",
+              "vlan 30",
+              "name Pedagogus",
+              "vlan 100",
+              "name Ugyvitel"
+             )
+    ssh.send_config_set(vlanok)
+
+
+
+
+try:
+    with ConnectHandler(**login_adatok)as kapcsolat:
+        vlan(kapcsolat)
+
+
+except Exception as ex:
+    print(f"Csatlakozási hiba: {ex}")
+
+
